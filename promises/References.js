d9931667e340d16825f5ca69a5e96eec6c4d698f
bill fero <billfero@gmail.com>
2017-05-24 16:23:15 +0100

    add sol to lesson one fetch api quiz test

---
+++ promises/References.js
@@ -0,0 +1,10 @@
+/*
+	References:
+	[1] https://developers.google.com/web/fundamentals/getting-started/primers/promises (Promises)
+	[2] https://developers.google.com/web/fundamentals/getting-started/primers/async-functions (Async functions)
+	[3] https://developers.google.com/web/fundamentals/getting-started/primers/service-workers (Service Workers)
+	[4] https://davidwalsh.name/fetch (Fetch Api)
+	[5] https://github.com/udacity/exoplanet-explorer/tree/xhr-solution (exoplanet repo)
+	
+
+ */
